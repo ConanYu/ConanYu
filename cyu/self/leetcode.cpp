556e178bdded255deec4f223488ad2c64927d071
ConanYu <c.zing@outlook.com>
2020-12-20 18:51:48 +0800

    feat: 新增自用的模板

---
+++ cyu/self/leetcode.cpp
@@ -0,0 +1,29 @@
+/**
+ * @author yuzining
+ * @date $CURRENT_YEAR/$CURRENT_MONTH/$CURRENT_DATE $CURRENT_HOUR:$CURRENT_MINUTE"
+ */
+ 
+#ifdef local
+#include "D:\VSCODE\local.hpp"
+template<typename T>
+ostream &operator<<(ostream &out, const vector<T> &vec) {
+  bool isPrint = false;
+  for (auto &x : vec) {
+    out << (isPrint ? ',' : '[') << x;
+    isPrint = true;
+  }
+  out << ']';
+  return out;
+}
+#else
+#define debug(...) do { } while(false)
+#endif
+ 
+ 
+ 
+#ifdef local
+void Main() {
+  Solution o;
+  cout << o.solve(xxx) << '\n';
+}
+#endif
---
+++ cyu/self/yuzining.cpp
@@ -0,0 +1,22 @@
+/**
+ * @author yuzining
+ * @date $CURRENT_YEAR/$CURRENT_MONTH/$CURRENT_DATE $CURRENT_HOUR:$CURRENT_MINUTE
+ */
+ 
+#ifdef local
+#include "D:\VSCODE\local.hpp"
+#else
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(...) do { } while(0)
+void Main();
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+  Main();
+  return 0;
+}
+#endif
+ 
+void Main() {
+  
+}
