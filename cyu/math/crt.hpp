0ce730fdda1d98c081a59fd0e695ca266b609062
ConanYu <c.zing@outlook.com>
2020-12-20 13:36:34 +0800

    feat: 新增exgcd模板和crt模板

---
+++ cyu/math/crt.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+#include "cyu/math/exgcd.hpp"
+
+namespace cyu {
+
+// v里每个std::pair<Type, Type>中first为被模数，second为模数
+template<typename Type>
+std::pair<Type, Type> Crt(const std::vector<std::pair<Type, Type>>& v) {
+  Type a = 1, r = 0;
+  const int n = v.size();
+  for (int i = 0; i < n; i++) {
+    Type x, y; 
+    std::tie(x, y, std::ignore) = ExGcd(a, v[i].first);
+    Type g = a * x + v[i].first * y;
+    if ((v[i].second - r) % g != 0) return std::make_pair(-1, -1);
+    const Type p = v[i].first / g;
+    Type t = (x * ((v[i].second - r) / g)) % p;
+    if (t < 0) t += p;
+    r += t * a;
+    a *= p;
+  }
+  return std::make_pair(a, r);
+}
+
+}  // namespace cyu
---
+++ cyu/math/exgcd.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+#include <tuple>
+
+namespace cyu {
+
+// find a, b, g that a * x + b * y = g
+// g = gcd(x, y)
+// return std::tuple<a, b, g>
+template<typename Type = int64_t>
+std::tuple<Type, Type, Type> ExGcd(const Type& x, const Type& y) {
+  if (y == 0) return std::make_tuple(1, 0, x);
+  Type a, b, g;
+  std::tie(a, b, g) = ExGcd(y, x % y);
+  return std::make_tuple(b, a - x / y * b, g);
+}
+
+}  // namespace cyu
