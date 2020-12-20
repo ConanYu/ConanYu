c6fb85313a358389cfb5f1ac2d24e59a41f86921
ConanYu <c.zing@outlook.com>
2020-12-20 13:06:08 +0800

    fix: 修复STable的bug

--- cyu/data_structure/s_table.hpp
+++ cyu/data_structure/s_table.hpp
@@ -8,7 +8,7 @@ namespace detail {
 template <typename Type, typename Comp = std::less<Type>>
 class InnerMerger {
  public:
-  Type operator()(const Type& lhs, const Type& rhs) {
+  Type operator()(const Type& lhs, const Type& rhs) const {
     return cmp(lhs, rhs) ? lhs : rhs;
   }
  private:
@@ -17,10 +17,10 @@ class InnerMerger {
 
 }  // namespace detail
 
-template<typename Type, typename Comp = detail::InnerMerger<Type>>
+template<typename Type, typename Merger = detail::InnerMerger<Type>>
 class STable {
  public:
-  explicit STable(const vector<Type>& arr) : cmp() {
+  explicit STable(const vector<Type>& arr) : merger() {
     const int n = static_cast<int>(arr.size());
     table.assign(n, vector<Type>(Log2(n) + 1));
     for (int i = 0; i < n; i++) {
@@ -29,14 +29,14 @@ class STable {
     for (int j = 1; (1 << j) <= n; j++) {
       for (int i = 0; i + (1 << j) - 1 < n; i++) {
         const Type x = table[i][j - 1], y = table[i + (1 << (j - 1))][j - 1];
-        table[i][j] = cmp(x, y) ? x : y;
+        table[i][j] = merger(x, y);
       }
     }
   }
   Type Query(int l, int r) const {
     const int k = Log2(r - l + 1);
     const Type x = table[l][k], y = table[r - (1 << k) + 1][k];
-    return cmp(x, y) ? x : y;
+    return merger(x, y);
   }
   Type operator()(int l, int r) const {
     return Query(l, r);
@@ -45,7 +45,7 @@ class STable {
   int Log2(int n) const {
     return 31 - __builtin_clz(static_cast<unsigned int>(n));
   }
-  const Comp cmp;
+  const Merger merger;
   std::vector<std::vector<Type>> table;
 };
 
